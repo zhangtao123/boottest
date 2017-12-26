79fb6d21ee0632443d2bfd5221e5e6e9aa8251be
xuyibo <xuyibo@anji-plus.com>
2017-12-26 13:44:32 +0800

    update

---
+++ src/main/java/com/example/boottest/constant/Constants.java
@@ -0,0 +1,5 @@
+package com.example.boottest.constant;
+
+public class Constants {
+    public static final String UPLOAD_FILE_PATH="C:/Users/Yibo/Documents";
+}
--- src/main/java/com/example/boottest/controller/TestController.java
+++ src/main/java/com/example/boottest/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.boottest.controller;
 
 import com.example.boottest.service.GetAllBrand;
-import com.example.boottest.util.FileUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +9,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+import static com.example.boottest.constant.Constants.UPLOAD_FILE_PATH;
 
 @RestController
-public class Test {
+public class TestController {
     @Resource
     private GetAllBrand getAllBrand;
 
@@ -24,22 +26,19 @@ public class Test {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView goHome() {
-        System.out.println("index");
         return new ModelAndView("index");
     }
 
     @RequestMapping(value = "/upFile", method = RequestMethod.POST)
-    public void upFile(@RequestParam("myFile") MultipartFile file, HttpServletRequest request) {
-
-        String contentType = file.getContentType();
-        String fileName = file.getOriginalFilename();
-        System.out.println("fileName-->" + fileName);
-        System.out.println("getContentType-->" + contentType);
-        String filePath = request.getSession().getServletContext().getRealPath("file/");
+    public Object upFile(@RequestParam("myFile") MultipartFile uploadFile) {
+        String fileName = uploadFile.getOriginalFilename();
+        File file = new File(UPLOAD_FILE_PATH, fileName);
         try {
-            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
-        } catch (Exception e) {
+            uploadFile.transferTo(file);
+            return "success";
+        } catch (IOException e) {
             e.printStackTrace();
-       }
+            return "failed";
+        }
     }
 }
