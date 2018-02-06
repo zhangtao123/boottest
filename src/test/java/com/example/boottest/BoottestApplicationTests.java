6925383ed27cdedacb1fd2a13a028d0023a388ed
xuyibo <xuyibo@anji-plus.com>
2018-02-06 12:44:33 +0800

    代码精简

--- src/test/java/com/example/boottest/BoottestApplicationTests.java
+++ src/test/java/com/example/boottest/BoottestApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.boottest;
 
+import com.example.boottest.dao.DealerDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoottestApplicationTests {
+    @Resource
+    private DealerDao dealerDao;
 
+    @Test
+    public void testDealerDao() {
+        System.out.println(dealerDao.selectByPrimaryKey(1L));
+    }
 }
