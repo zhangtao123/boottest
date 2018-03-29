b9c168c9d40cf89c28b52c6c7c93c07cefea724a
XuYibo <xuyibo@anji-plus.com>
2018-03-29 10:52:59 +0800

    代码优化

---
+++ src/main/java/com/example/boottest/config/Swagger2.java
@@ -0,0 +1,36 @@
+package com.example.boottest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.boottest.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("Yibo的API文档")
+                .termsOfServiceUrl("https://www.baidu.com/")
+                .contact(new Contact("Yibo", "", ""))
+                .version("1.0")
+                .build();
+    }
+}
